49994ac01486b0ea241516ec1d991b7bd89819d1
WAHIB MOQBEL <75446834+Wahib-eng@users.noreply.github.com>
2023-11-18 20:46:10 +0300

    Create dıctıonary.py

---
+++ dıctıonary.py
@@ -0,0 +1,18 @@
+# 
+dic1 = {"name" : "wahib", "age ": 28}
+print(dic1.keys())
+print(dic1.values())
+print(dic1)
+
+
+
+c = 2.019
+ 
+print(int(c))
+print(c)
+print(str(c))
+print(repr(c))
+print(eval(str(c)))
+
+
+
