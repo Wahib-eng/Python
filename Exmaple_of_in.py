cdee34485b898427f415be82123db718ab02db64
WAHIB MOQBEL <75446834+Wahib-eng@users.noreply.github.com>
2024-01-03 20:59:36 +0300

    Create Exmaple_of_in.py

---
+++ Exmaple_of_in.py
@@ -0,0 +1,10 @@
+# Written by Wahib ^ ^
+a = [1,2,3,4,5]
+
+b = int ( input("Enter a number you searching for "))
+
+
+if b in a : 
+    print(f"{b} is found in the list ")
+else : 
+    print (f"{b} is found in the list ")
